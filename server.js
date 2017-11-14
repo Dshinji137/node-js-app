be57f250883a505a70325abbc7a4e792d1769aeb
Hengzhi Ding <Dshinji@dyn-160-39-223-225.dyn.columbia.edu>
2017-11-14 17:14:29 -0500

    second

--- server.js
+++ server.js
@@ -21,15 +21,12 @@ app.use((req,res,next) => {
   next();
 });
 
-
 //app.use((req,res,next) => {
 //  res.render('maintenance.hbs');
 //  next();
 //});
 
 app.use(express.static(__dirname+'/public'));
-
-
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -48,6 +45,12 @@ app.get('/', (req,res) => {
 app.get('/about',(req,res) => {
   res.render('about.hbs',{
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
